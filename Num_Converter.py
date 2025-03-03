da0fef30cfc5f92f142156edb41c231f5b8cc722
Suraj179 <02240179.cst@rub.edu.bt>
2025-03-03 17:17:11 +0600

    Update Num_Converter.py
    function to convert from base to decimal  is added

--- Num_Converter.py
+++ Num_Converter.py
@@ -1,6 +1,7 @@
 
-
+dic={0:"0",1:"1",2:"2",3:"3",4:"4",5:"5",6:"6",7:"7",8:"8",9:"9",10:"A",11:"B",12:"C",13:"D",14:"E",15:"F"}
 def decimal_base(number:float,base:int):
+   
     number1=int(number)
     round_of_number2=len(str(number))-len(str(number1))
     number2=round(number%1,round_of_number2)
@@ -12,31 +13,45 @@ def decimal_base(number:float,base:int):
     answer1=""
     while number1!=0:
         remainder=int(number1%base)
-        remainder_str=str(remainder)
+        remainder_str=dic[remainder]
         answer1=remainder_str+answer1
         number1=(number1-remainder)/base
     
     #converting decimal part
-    answer2="0."
+    answer2="."
     while number2!=0:
         product=number2*base
         pro_int=int(product)
-        pro_str=str(pro_int)
+        pro_str=dic[pro_int]
         answer2+=pro_str
         number2=product-pro_int
-
-    answer1=int(answer1)
-    answer2=float(answer2)
-    if answer1!=0:answer+=answer1
-    if answer2!=0:answer+=answer2
+    try:
+        answer1=int(answer1)
+        answer2=float(answer2)
+        if answer1!=0:answer+=answer1
+        if answer2!=0:answer+=answer2
+    except ValueError:
+        answer=""
+        if answer1!="":answer=answer+answer1
+        if answer2!=".":answer=answer+answer2
     return answer
 # num=float(input("Enter the number: "))
 # base=int(input("Enter the base: "))
-print(0==0.0)
-i=True
-while i:
-    num=float(input("Enter the number: "))
-    base=int(input("Enter the base: ")) 
-    print(decimal_base(num,base))
-    i=input("enter truth value: ")
+# print(decimal_base(num,base))
 
+def base_decimal(number,base):
+    #get the maximum exponent or highest power
+    integer=number.split(".")[0]
+    highest_power=len(integer)-1
+    print(highest_power)
+    dic2 = {v: k for k, v in dic.items()}
+    answer=int()
+    for i in number:
+        if i==".":continue
+        num=dic2[i]
+        answer+=num*base**highest_power
+        highest_power-=1
+    
+    print(answer)
+
+base_decimal("154724.6543177",8)
